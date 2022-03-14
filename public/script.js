bf1964e3223750d0247ee4685b6372a188cfa9fd
juanddiaz13 <juanddiaz13@gmail.com>
2022-03-14 18:54:33 -0500

    servicio 1 y 2 funcionales cuando el usuario existe

--- public/script.js
+++ public/script.js
@@ -7,18 +7,48 @@ const message = document.getElementById('message');
 enterButton.addEventListener('click', (event) => {
   //Implementar lógica del button submit
   alert('Implementar lógica del button submit');
-  getUser('james');
+  let element = document.getElementById("inputText").value;
+  getUser(element);
+  
   event.preventDefault();
 });
+
 
 /**
  * Llamado al backend con queryParam
  * @param {*} username
  */
 async function getUser(username) {
-  const resp = await fetch(`api/users/${username}`);
+  
+    const resp = await fetch(`api/users/${username}`);
   const data = await resp.json();
   console.log('data from back', data);
+  let elemento = document.getElementById("card-body");
+  let actual = `
+    <p>Username: ${data.username}</p> <br>
+    <p>Friends: ${data.friends}</p> <br>
+    <p>Plays: ${data.plays}</p> <br>
+
+  `;
+  console.log(data.tracks)
+  let nuevoArreglo = [];
+
+  data.tracks.forEach(element => {
+    
+  });
+
+
+
+  console.log("arreglo tracks")
+
+  let elementoTracks = document.getElementById("body-table");
+  let actual2 = `
+    <p>Tracks: ${data.tracks}</p> <br>
+  `;  
+  elementoTracks.innerHTML=actual2;
+  elemento.innerHTML=actual;
+  
+  
   //printValues(data);
 }
 
--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,45 @@
-const getUser = async () => {
+const axios  = require('axios');
+const fetch = require('node-fetch');
+
+
+//Hago dos fetch para obtener la informacion
+//Luego agrego la informacion correspondiente de cada fetch a un objeto que retorno
+//
+const getUser = async (usuario) => {
   //TODO user logic
-  return { message: 'To be implemented' };
+  let totalAmigos = 0;
+  let totalPPP = 0;
+  let usuarioObjeto = {};
+
+  const res = await axios.get('https://mauvelous-leopard-5257.twil.io/friend-detail?username='+usuario)
+
+  res.data.friends.forEach(element => {
+    totalAmigos+=1;
+});
+  usuarioObjeto = {
+    username: "joe_example",
+    plays: totalPPP,
+    friends: totalAmigos,
+    tracks: [],
+    uri: res.data.uri
+    };
+
+  const res1 = await axios.get('https://mauvelous-leopard-5257.twil.io/plays-detail?username='+usuario)
+
+    res1.data.plays.forEach(element => {
+      totalPPP+=1;
+  });
+  usuarioObjeto = {
+    username: usuario,
+    plays: totalPPP,
+    friends: totalAmigos,
+    tracks: res1.data.plays,
+    uri: res.data.uri
+    };
+  console.log(usuarioObjeto)
+  
+
+  return usuarioObjeto;
 };
 
 module.exports = { getUser };
